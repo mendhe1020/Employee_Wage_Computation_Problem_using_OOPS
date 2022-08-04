7d5b329ffc9e0c0dd8e7ec068a26173a95534a2a
Anurag1020 <anurag.mendhe14@gmail.com>
2022-08-04 15:00:04 +0530

    Check Employee is Present or Absent

---
+++ EmployeeWage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation_Problem_using_OOPS
+{
+    public class EmployeeWage
+    {
+
+        public int Empis;
+        public void EmpWage()
+        {
+            Random random = new Random();
+            int Empis = random.Next(2);
+
+            if (Empis == 0)
+            {
+                Console.WriteLine("Emploee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emploee is Abcent");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Employee_Wage
+﻿using Employee_Wage_Computation_Problem_using_OOPS;
+
+namespace Employee_Wage
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem");
+
+
+
+            EmployeeWage obj = new EmployeeWage();
+
+            obj.EmpWage();
+
+
+
         }
     }
 }
