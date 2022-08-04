7eefdf51cbe234016761add2343dcf7d65f5f2cb
Anurag1020 <anurag.mendhe14@gmail.com>
2022-08-04 15:27:03 +0530

    Add Part time Employee & Wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -14,14 +14,20 @@ namespace Employee_Wage_Computation_Problem_using_OOPS
         {
             int WagePrHr = 20;
             int FullDayHr = 8;
+            int PartTimeHr = 4;
 
             Random random = new Random();
-            int Empis = random.Next(2);
+            int Empis = random.Next(3);
 
             if (Empis == 0)
             {
                 Console.WriteLine("Emploee is Present");
                 Console.WriteLine(WagePrHr * FullDayHr);
+            }
+            else if (Empis == 1)
+            {
+                Console.WriteLine("Emp is part time present");
+                Console.WriteLine(WagePrHr * PartTimeHr);
             }
             else
             {
